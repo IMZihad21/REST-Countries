6e494d57ed85bd7535042a7fe99627cd9b3b82f3
ZèD <imzihad@gmail.com>
2021-09-24 05:03:28 +0000

    Fixup API and props

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -5,14 +5,14 @@ import './Countries.css';
 const Countries = () => {
     const [ countries, setCountries ] = useState([]);
     useEffect(() => {
-        fetch('https://restcountries.com/v3/all')
+        fetch('https://restcountries.com/v2/all')
             .then(res => res.json())
             .then(data => setCountries(data));
     }, [])
     return (
         <div>
             {
-                countries.map(country => <Country name={country.name}></Country>)
+                countries.map(country => <Country name={country.name} capital={country.capital} population={country.population}></Country>)
             }
         </div>
     );
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -4,6 +4,8 @@ const Country = (props) => {
     return (
         <div>
             <h3>{props.name}</h3>
+            <p>Capital is : {props.capital}</p>
+            <p>Polpulation is: {props.population}</p>
         </div>
     );
 };
